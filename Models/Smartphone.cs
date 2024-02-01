08e32d5d53621227dae71b6a9812b3edd8376f81
Lennon <lennon_lxs@hotmail.com>
2024-02-01 17:39:08 -0300

    Cria as classes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,14 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public string Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
-
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
